5c1b907e1a5ccaacd2ea272a85db3e89202e5e5c
SivaBalaji <151618924+SivaBalaji-AR@users.noreply.github.com>
2025-03-23 00:04:26 +0530

    Update fetchContests.js

--- src/Functions/fetchContests.js
+++ src/Functions/fetchContests.js
@@ -1,6 +1,6 @@
 const fetchContests = async () => {
     try {
-      const response = await fetch("http://localhost:5000/api/upcoming-contests");
+      const response = await fetch("https://algo-hub-six.vercel.app/api/upcoming-contests");
       if (!response.ok) throw new Error("Failed to fetch contests");
   
       const data = await response.json();
